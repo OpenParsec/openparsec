76e1d0e703efee8b132d9b79acb86fee42213819
Alex Szpakowski <amaranth72@gmail.com>
2013-01-22 19:16:06 -0400

    Tidied up premake script a bit

--- platforms/premake/premake4.lua
+++ platforms/premake/premake4.lua
@@ -1,101 +1,97 @@
 --[[
-    useage example:
-        premake4 gmake && make config=release server
+	usage example:
+		premake4 gmake && make config=release server
 
-    only designed to work with Linux for now
-    
-    TODO:
-        get shit working
-        copy data files to proper locations?
-        optional: get non-linux shit working
+	only works with Linux for now
 ]]
 
 
 local paths = {
-    libraries = "../../src/libraries",
-    libparsec = "../../src/libparsec",
-    parsec = "../../src/parsec",
-    parsecserver = "../../src/parsec_server",
-    parsecroot = "../../../../parsec_root", -- adnauseum("dotdotslash")
+	root = path.getabsolute("../../"), -- root openparsec folder
+	premakefolder = path.getabsolute(""),
+}
+
+paths.parsecroot = path.join(paths.root, "parsec_root")
+
+
+local srcpaths = {
+	libraries = "../../src/libraries",
+	libparsec = "../../src/libparsec",
+	parsec = "../../src/parsec",
+	parsecserver = "../../src/parsec_server",
 }
 
 
 solution "ParsecSolution"
-    language "C++"
-    platforms { "x32" }
-    includedirs { "../../src/libraries" }
-    
-    configurations { "Debug", "Release" }
-    
-    configuration "Debug"
-        defines { "DEBUG" }
-        flags "Symbols"
-            
-    configuration "Release"
-        flags "Optimize"
-    
-    configuration "linux"
-        includedirs { "/usr/include", "/usr/local/include", "/opt/include" }
-    
+	language "C++"
+	platforms { "x32" }
+	includedirs { srcpaths.libraries }
+	
+	configurations { "Debug", "Release" }
+	
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags "Symbols"
+			
+	configuration "Release"
+		flags "Optimize"
+	
+	configuration "linux"
+		includedirs { "/usr/include", "/usr/local/include", "/opt/include" }
+	
 
 project "client"
-    kind "WindowedApp"
-    location "build/client"
-    targetdir "build/client"
-    targetname "parsec"
-    
-    defines { "PARSEC_CLIENT" }
-    
-    includedirs {
-        paths.libparsec.."/include",
-        paths.parsec.."/include",
-    }
-    
-    files {
-        paths.libraries.."/**",
-        paths.libparsec.."/**",
-        paths.parsec.."/**",
-    }
-    configuration "not windows"
-        -- These are apparently windows thingies
-        excludes { paths.parsec.."/**.rc" }
-    
-    configuration "linux"
-        links { "SDL", "SDL_mixer", "GL" }
-        postbuildcommands 
-        {
-            "cp parsec "..paths.parsecroot.."/client/",
-        }
+	kind "WindowedApp"
+	location "build/client"
+	targetdir "build/client"
+	targetname "Parsec"
+	
+	defines { "PARSEC_CLIENT" }
+	
+	includedirs {
+		srcpaths.libparsec.."/include",
+		srcpaths.parsec.."/include",
+	}
+	
+	files {
+		srcpaths.libraries.."/**",
+		srcpaths.libparsec.."/**",
+		srcpaths.parsec.."/**",
+	}
+	
+	configuration "not windows"
+		-- These are apparently windows thingies
+		excludes { srcpaths.parsec.."/**.rc" }
+	
+	configuration "linux"
+		targetname "parsec"
+		links { "SDL", "SDL_mixer", "GL" }
+		postbuildcommands {
+			("cp parsec %q"):format(paths.parsecroot.."/client/"),
+		}
+
 
 project "server"
-    kind "ConsoleApp"
-    location "build/server"
-    targetdir "build/server"
-    targetname "parsec_server"
-    
-    defines { "PARSEC_SERVER" }
-    
-    -- TODO lots of crap probably
-    -- needs to link with 
-    
-    includedirs {
-        paths.libparsec.."/include",
-        paths.parsecserver.."/include",
-    }
-    
-    files {
-        paths.libparsec.."/**",
-        paths.parsecserver.."/**",
-    }
+	kind "ConsoleApp"
+	location "build/server"
+	targetdir "build/server"
+	targetname "parsec_server"
+	
+	defines { "PARSEC_SERVER" }
+	
+	includedirs {
+		srcpaths.libparsec.."/include",
+		srcpaths.parsecserver.."/include",
+	}
+	
+	files {
+		srcpaths.libparsec.."/**",
+		srcpaths.parsecserver.."/**",
+	}
 
-    configuration "linux"
-        links { "ncurses" }
-        postbuildcommands 
-        {
-            "cp parsec_server "..paths.parsecroot.."/server/",
-        }
-    
-
-
-
+	configuration "linux"
+		links { "ncurses" }
+		postbuildcommands {
+			("cp parsec_server %q"):format(paths.parsecroot.."/server/"),
+		}
 
